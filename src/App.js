2632daef14baa16b44851378f8ab0889edf2ed9b
Daniel Talisman <dantalisman@gmail.com>
2021-04-05 23:11:49 +0300

    basic routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Home from "./components/Home";
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
